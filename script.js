96681c7bcb327bd7487f0d1dbf645a26664b68a2
Brahner <brachneq@gmail.com>
2022-09-02 21:03:14 +0400

    add function show all todo list

--- script.js
+++ script.js
@@ -32,6 +32,7 @@ document.querySelector('.add-todo__btn').addEventListener('click', ()=>{
         list.push(obj);
     }
     getNumTodo();
+    showTodoList();
 });
 
 
@@ -50,8 +51,28 @@ function getNumDone(){
     
 }
 
+function showTodoList(){
+    let wrapper = document.querySelector('.todo-list__wrapper');
+    wrapper.querySelectorAll('.todo-list__item').forEach(n => n.remove());
+    for(let elem of list){
+        let item = document.createElement('div');
+        item.className = 'todo-list__item';
 
+        let checkbox = document.createElement('input');
+        checkbox.setAttribute('type', 'checkbox');
+        checkbox.className = 'todo-list__checkbox';
 
+        let text = document.createElement('span');
+        text.className = 'todo-list__text';
+        text.textContent = `${elem['name']}`;
+
+        wrapper.appendChild(item);
+        item.appendChild(checkbox);
+        item.appendChild(text);
+    }
+}
+
+showTodoList()
 
 
 
