48d509e51f87cd7ab1d5c3496f8c6f10dad17680
Brahner <brachneq@gmail.com>
2022-09-02 18:03:14 +0400

    Add fix more and done tode. Add new function addtask

--- script.js
+++ script.js
@@ -16,23 +16,28 @@ const list = [
     }
 ];
 
-document.querySelector('.header__more').textContent = `${list.length} more to do,`;
-document.querySelector('.header__done').textContent = `${getNumDone()} done`
-
-
-
-
-/* const getNumDone = ()=>{
+getNumTodo();
+document.querySelector('.header__done').textContent = `${getNumDone()} done`;
+document.querySelector('.add-todo__btn').addEventListener('click', ()=>{
+    let newTask = document.querySelector('input').value;
     let num = 0;
     for(let elem of list){
-        for(let key in elem){
-            if(elem[key] === 'Done'){
-                num++;
-            }
+        if(elem['name'] === newTask){
+            num++
         }
     }
-    return num;
-} */
+    if(num === 0){
+        let obj = {};
+        obj['name'] = newTask, obj['status'] = 'To Do', obj['priority'] = 'low';
+        list.push(obj);
+    }
+    getNumTodo();
+});
+
+
+function getNumTodo(){
+    document.querySelector('.header__more').textContent = `${list.length} more to do,`;
+}
 
 function getNumDone(){
     let num = 0;
@@ -44,6 +49,8 @@ function getNumDone(){
     return num;
     
 }
+
+
 
 
 
